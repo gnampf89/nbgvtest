ccda8b58e3f335899efc38cd061c965528c50260
Michael Nowagk <michael.nowagk@linde.com>
2022-07-29 11:28:46 +0200

    minimal test solution

---
+++ FrameworkTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace FrameworkTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            Console.WriteLine(fvi.ProductVersion);
+        }
+    }
+}
